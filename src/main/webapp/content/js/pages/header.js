0dc30f88cc52bb4e462c03d0b32358783ee91ae7
gustavosk <gustavo_sk@live.com>
2015-06-18 23:16:32 -0300

    criado select para login

--- src/main/webapp/content/js/pages/header.js
+++ src/main/webapp/content/js/pages/header.js
@@ -1,12 +1,13 @@
 function login(){
-	var login = $
+	var login = $('#userLogin')[0].value;
+	var password = $('#passLogin')[0].value;
 	
-	var url = '/Corvette/cadastroCliente?username='+ login +'&password='+ password;
+	var url = '/Corvette/login?username='+ login +'&password='+ password;
 	$.ajax({
 		type: "POST",
 		url: url,
 		success: function(result) {
-			
+			alert(result);
 		},
 	});
 }
